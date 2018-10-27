e7cf4f9c15d979ca66917bf3735fc210f64cac29
luoqiangwei <luoqiangwei@live.cn>
2018-10-27 16:21:13 +0800

    new puzzle.

---
+++ 1015/gemstone_rewards.c
@@ -0,0 +1,21 @@
+ #include <stdio.h>
+ 
+ int main(int argc, char* argv)
+ {
+    int col = 0;
+    int row = 0;
+    int x = 0;
+    int y = 0;
+    int r = 0;
+    scanf("%d%d", &row, &col);
+    scanf("%d%d%d", &x, &y, &r);
+    for (int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            
+        }
+    }
+
+    return 0;
+ }
