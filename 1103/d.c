03b95e35a5ec9e6efec671a6110335ce5d9fb978
luoqiangwei <luoqiangwei@live.cn>
2018-11-18 13:05:17 +0800

    1118.

---
+++ 1103/d.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+fun(char *w, int n)
+{
+    char t, *s1, *s2;
+    s1 = w;
+    s2 = w + n - 1;
+    while (s1 < s2)
+    {
+        t = *s1++;
+        *s1 = *s2--;
+        *s2 = t;
+    }
+}
+int main()
+{
+    char *p;
+    p = "1234567";
+    fun(p, strlen(p));
+    puts(p);
+    return 0;
+}
